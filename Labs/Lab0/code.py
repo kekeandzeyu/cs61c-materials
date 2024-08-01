8a1e71fcea77b92fde634393259d8383d08f2d7e
kekeandzeyu <kekeandzeyu@gmail.com>
2024-08-01 12:29:57 +0800

    Finish Lab 0 and Lab 1!

---
+++ Labs/Lab0/code.py
@@ -0,0 +1,22 @@
+def get_airspeed_velocity_of(unladen_swallow):
+    if unladen_swallow.type == "african":
+        return  # redacted
+    elif unladen_swallow.type == "european":
+        return  # redacted
+
+
+# pretend there's code here...
+
+def fizzbuzz(num):
+    if num % 15 == 0:
+        print(f"{num}: FizzBuzz")
+    elif num == 3:
+        print(f"{num}: fizz")
+    elif num == 5:
+        print(f"{num}: buzz")
+
+
+for i in range(1, 20):
+    fizzbuzz(i)
+
+# pretend there's code here...
