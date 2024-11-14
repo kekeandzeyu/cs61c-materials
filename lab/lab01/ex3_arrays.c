cdab8e87bf8a427ad947b3e5520f1516d405145e
kekeandzeyu <kekeandzeyu@gmail.com>
2024-11-14 10:43:14 +0800

    update lab01!

---
+++ lab/lab01/ex3_arrays.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Creates an integer with value 5
+    // Note: int8_t is a numerical datatype that takes up 1 byte of memory
+    int8_t x = 5;
+
+    // Create an int8_t array of size 4
+    int8_t some_array[4];
+    printf("address of the start of the array: %p\n", some_array);
+
+    // Compute the address of the element at index 2 (0-indexed)
+    int8_t* ptr_to_idx_2 = &some_array[2];
+    printf("address of index 2: %p\n", ptr_to_idx_2);
+
+    // Store the value 10 at index 2, using ptr_to_idx_2
+    *ptr_to_idx_2 = 10;
+
+    // Print the value at index 2
+    // Hint: this blank should be the same as the previous blank
+    //       please don't hard code 10
+    printf("value at index 2: %d\n", *ptr_to_idx_2);
+
+    return 0;
+}
---
+++ lab/lab01/ex7_structs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structs allow you to hold data items of different types in a single variable
+// Struct definitions can be used to declare a struct variable within your program
+// Struct definitions are typically done outside of a function
+struct Student {
+    int id;
+    char* name;
+};
+
+int main() {
+    // Declare a variable student of type struct Student
+    // Note: this struct is stored on the stack
+    struct Student student;
+
+    // Print out the size of a struct Student
+    // While this may seem out of place now, it will be useful in the future!
+    // Hint: there's an operator that can calculate this for you!
+    printf("Size of a struct Student: %lu bytes\n", sizeof(struct Student));
+
+    // Set student's id field to 5
+    // Hint: the dot notation accesses a struct's fields
+    student.id = 5;
+
+    // Print out student's id field
+    printf("Student's ID: %d\n", student.id);
+
+    return 0;
+}
