fcdccb453bb57cdfbdbe025b1c9458f914ffea3b
kekeandzeyu <kekeandzeyu@gmail.com>
2024-11-15 19:00:07 +0800

    finish up lab 2!

--- lab/lab02/ex7_vector.c
+++ lab/lab02/ex7_vector.c
@@ -40,33 +40,33 @@ vector_t *bad_vector_new() {
 /* TODO: uncomment the code that is preceded by // */
 vector_t *vector_new() {
     /* Declare what this function will return */
-    // vector_t *retval;
+    vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    // retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t));
 
     /* Check our return value to make sure we got memory */
-    // if (/* YOUR CODE HERE */) {
-    //     allocation_failed();
-    // }
+    if (retval == NULL) {
+        allocation_failed();
+    }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    // retval->size = /* YOUR CODE HERE */;
-    // retval->data = /* YOUR CODE HERE */;
+    retval->size = 1;
+    retval->data = malloc(sizeof(int) * retval->size);
 
     /* Check the data attribute of our vector to make sure we got memory */
-    // if (/* YOUR CODE HERE */) {
-    //     free(retval);				//Why is this line necessary?
-    //     allocation_failed();
-    // }
+    if (retval->data == NULL) {
+        free(retval);				//Why is this line necessary? => Prevent memory leak!
+        allocation_failed();
+    }
 
     /* Complete the initialization by setting the single component to zero */
-    // /* YOUR CODE HERE */ = 0;
+    retval->data[0] = 0;
 
     /* and return... */
-    return NULL; /* UPDATE RETURN VALUE */
+    return retval; /* UPDATE RETURN VALUE */
 }
 
 /* Return the value at the specified location/component "loc" of the vector */
@@ -81,14 +81,17 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    /* YOUR CODE HERE */
-    return 0;
+    if(loc >= v->size) {
+        return 0;
+    }
+    return v->data[loc];
 }
 
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
-    /* YOUR CODE HERE */
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector, allocating additional memory if necessary. 
@@ -97,6 +100,19 @@ void vector_set(vector_t *v, size_t loc, int value) {
     /* What do you need to do if the location is greater than the size we have
      * allocated?  Remember that unset locations should contain a value of 0.
      */
-
-    /* YOUR CODE HERE */
+    if(loc >= v->size) {
+        size_t new_size = loc + 1;
+        int *new_data = realloc(v->data, sizeof(int) * new_size);
+        if(new_data == NULL) {
+            allocation_failed();
+        }
+        v->data = new_data;
+        for(size_t i = v->size; i < new_size; i++) {
+            v->data[i] = 0;
+        }
+        v->size = new_size;
+        v->data[loc] = value;
+    } else {
+        v->data[loc] = value;
+    }
 }
--- lab/lab02/ex8_double_pointers.c
+++ lab/lab02/ex8_double_pointers.c
@@ -7,50 +7,50 @@ typedef struct {
 } Student;
 
 Student* create_student_1(int id) {
-  // TODO: allocate memory to store a Student struct
-  ________ student_ptr = malloc(________);
+    // Allocate memory to store a Student struct
+    Student* student_ptr = malloc(sizeof(Student));
 
-  // TODO: set student_ptr's id to the id argument of this function
-  ____________ = id;
+    // Set student_ptr's id to the id argument of this function
+    student_ptr->id = id;
 
-  return student_ptr;
+    return student_ptr;
 }
 
 void create_student_2(Student** student_double_ptr, int id) {
-  // TODO: fill the space that student_double_ptr points to with the address of
-  //       some memory large enough for a Student struct
-  // Hint: you may need to use the dereference operator here
-  ______________ = malloc(________);
+    // Fill the space that student_double_ptr points to with the address of
+    //       some memory large enough for a Student struct
+    // Hint: you may need to use the dereference operator here
+    *student_double_ptr = malloc(sizeof(Student));
 
-  // TODO: set student_double_ptr's id to the id argument of this function
-  ____________ = id;
+    // Set student_double_ptr's id to the id argument of this function
+    (*student_double_ptr)->id = id;
 }
 
 
 int main() {
-  // TODO: use create_student_1 to create a pointer to a Student struct
-  //       where the student has id of 5
-  ______ student1_ptr = ______(______);
+    // Use create_student_1 to create a pointer to a Student struct
+    //       where the student has id of 5
+    Student* student1_ptr = create_student_1(5);
 
-  // TODO: print the id of the student that student1_ptr points to
-  printf("Student 1's ID: %d\n", ______);
+    // Print the id of the student that student1_ptr points to
+    printf("Student 1's ID: %d\n", student1_ptr->id);
 
-  // TODO: create a pointer that can point to a Student struct
-  //       do not allocate any memory
-  ______ student2_ptr;
+    // Reate a pointer that can point to a Student struct
+    //       do not allocate any memory
+    Student* student2_ptr;
 
-  // TODO: use create_student_2 to populate the student2_ptr
-  //       where the student has id of 6
-  // Hint: compare the type of student2_ptr with the type of
-  //       the argument for create_student_2
-  create_student_2(______, ______);
+    // Use create_student_2 to populate the student2_ptr
+    //       where the student has id of 6
+    // Hint: compare the type of student2_ptr with the type of
+    //       the argument for create_student_2
+    create_student_2(&student2_ptr, 6);
 
-  // TODO: print the id of the student that student2_ptr points to
-  printf("Student 2's ID: %d\n", ______);
+    // Print the id of the student that student2_ptr points to
+    printf("Student 2's ID: %d\n", student2_ptr->id);
 
-  // Free everything allocated with `malloc`
-  free(student1_ptr);
-  free(student2_ptr);
+    // Free everything allocated with `malloc`
+    free(student1_ptr);
+    free(student2_ptr);
 
-  return 0;
+    return 0;
 }
